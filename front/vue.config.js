38d6a119a1f290f6a4a8b55129ca779737df986f
shinyena <shinyena03@gmail.com>
2025-11-02 12:34:02 +0900

    axios 및 devServer 프록시 설정 추가

--- front/vue.config.js
+++ front/vue.config.js
@@ -2,6 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  devServer: {
+    proxy:{
+      "/api":{
+        target: "http://localhost:3000",
+        changeOrigin: true,     // ✅ 요청 헤더의 Origin을 백엔드 주소로 변경
+        secure: false,          // ✅ https 아닐 경우 false로 설정
+        ws: true,  
+      }
+    }
+  },
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
